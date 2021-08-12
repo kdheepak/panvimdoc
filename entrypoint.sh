a61abe824d666658828de6d64a6572cd066cd35e
Dheepak Krishnamurthy <me@kdheepak.com>
2021-08-12 00:41:03 -0600

    Update workflow

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-pandoc -t $GITHUB_WORKSPACE/scripts/panvimdoc.lua $GITHUB_WORKSPACE/$INPUTS_PANDOC -o $GITHUB_WORKSPACE/$INPUTS_VIMDOC
+pandoc -t $GITHUB_WORKSPACE/scripts/panvimdoc.lua $1 -o $2
