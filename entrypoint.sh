f56f060fbf09ea31024f37957c4b4d74843a92dd
Dheepak Krishnamurthy <me@kdheepak.com>
2021-08-12 22:49:18 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-pandoc --metadata=project:$1 --lua-filter scripts/include-files.lua -t scripts/panvimdoc.lua $2 -o $1.txt
+pandoc --metadata=project:$1 --lua-filter scripts/include-files.lua -t scripts/panvimdoc.lua $2 -o doc/$1.txt
