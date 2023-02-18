bd8c797cfdd51a20d8de0e4763dc4d28ef8baf01
Dheepak Krishnamurthy <me@kdheepak.com>
2023-02-18 18:41:07 -0500

    feat: Remove github emoji with demoji true ✨

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,24 +13,19 @@ function test_pandoc(text::String; toc = true, dedup_subheadings = false, treesi
   open(joinpath(CUR_DIR, "test.md"), "w") do file
     write(file, text)
   end
-  filters = "--lua-filter=$SCRIPTS_DIR/skip-blocks.lua --lua-filter=$SCRIPTS_DIR/include-files.lua"
+  filters = `--lua-filter=$SCRIPTS_DIR/skip-blocks.lua --lua-filter=$SCRIPTS_DIR/include-files.lua`
   if demojify
-    filters = "$filters --lua-filter=$SCRIPTS_DIR/remove-emojis.lua"
+    filters = `$filters --lua-filter=$SCRIPTS_DIR/remove-emojis.lua`
   end
   cd(ROOT_DIR) do
     run(
-      `$(Pandoc.pandoc()) --metadata=project:"test" --metadata=vimversion:"NVIM v0.8.0" --metadata=toc:$toc --metadata=description:"Test Description" --metadata=dedupsubheadings:$dedup_subheadings --metadata=treesitter:$treesitter  -t $SCRIPTS_DIR/panvimdoc.lua $CUR_DIR/test.md -o test/test.txt`,
+      `$(Pandoc.pandoc()) --metadata=project:"test" --metadata=vimversion:"NVIM v0.8.0" --metadata=toc:$toc --metadata=description:"Test Description" --metadata=dedupsubheadings:$dedup_subheadings --metadata=treesitter:$treesitter $filters -t $SCRIPTS_DIR/panvimdoc.lua $CUR_DIR/test.md -o test/test.txt`,
     )
   end
   doc = read(joinpath(ROOT_DIR, "test/test.txt"), String)
   rm(joinpath(ROOT_DIR, "test/test.txt"))
   rm(joinpath(CUR_DIR, "test.md"))
   return doc
-end
-
-if length(ARGS) == 0
-  push!(ARGS, "treesitter")
-  push!(ARGS, "demojify")
 end
 
 @testset ExtendedTestSet "All the tests" begin
